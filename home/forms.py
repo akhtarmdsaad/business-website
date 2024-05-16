812397bbced2de3cf87427cd129250ad591a962c
akhtarmdsaad <akhtarmdsaad@gmail.com>
2024-05-16 11:16:24 +0530

    Completed Website

--- home/forms.py
+++ home/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from .models import Order
 
 class OrderForm(forms.ModelForm):
-    
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control col-sm-6'
     class Meta:
         model = Order
         fields = ["materials","payment_mode"]
